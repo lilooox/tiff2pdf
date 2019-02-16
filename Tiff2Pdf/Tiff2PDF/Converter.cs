db101f32ea928dd451ed70e38f77106493675771
lilooox <754021838@qq.com>
2019-02-16 14:51:33 +0800

    change to consoleapp

---
+++ Tiff2Pdf/Tiff2PDF/Converter.cs
@@ -0,0 +1,78 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiff2PDF
+{
+    public class Converter
+    {
+        public static bool ConvertTiffToPdf(string inFilename, string outFilename)
+        {
+            bool result = false;
+
+            //iTextSharp.text.Rectangle pgSize = PageSize.LETTER;
+            iTextSharp.text.Rectangle pgSize = PageSize.LEGAL;
+
+            Bitmap bmp = new Bitmap(inFilename);
+
+            int totalPages = bmp.GetFrameCount(FrameDimension.Page);
+
+            float margin = 0.0f;
+            //float margin = 50.0f;
+            Document document = new Document(pgSize, margin, margin, margin, margin);
+
+            try
+            {
+                // creation of the different writers
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(outFilename, FileMode.Create));
+
+                // Which of the multiple images in the TIFF file do we want to load
+                // 0 refers to the first, 1 to the second and so on.
+                document.Open();
+                PdfContentByte cb = writer.DirectContent;
+                iTextSharp.text.Image img;
+
+                for (int ii = 0; ii < totalPages; ii++)
+                {
+                    bmp.SelectActiveFrame(FrameDimension.Page, ii);
+                    img = iTextSharp.text.Image.GetInstance(BitmapToBytes(bmp));
+                    img.ScalePercent(72f / bmp.HorizontalResolution * 100);
+                    img.SetAbsolutePosition(0, 0);
+                    cb.AddImage(img);
+                    document.NewPage();
+                }
+
+                document.Close();
+                document = null;
+                bmp.Dispose();
+                bmp = null;
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Failed: {0}", ex.ToString()));
+            }
+            return result;
+        }
+
+        private static byte[] BitmapToBytes(Bitmap bmp)
+        {
+            byte[] data = new byte[0];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bmp.Save(ms, ImageFormat.Png);
+                ms.Seek(0, 0);
+                data = ms.ToArray();
+            }
+            return data;
+        }
+
+    }
+}
--- Tiff2Pdf/Tiff2PDF/Program.cs
+++ Tiff2Pdf/Tiff2PDF/Program.cs
@@ -8,15 +8,41 @@ namespace Tiff2PDF
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        ///// <summary>
+        ///// The main entry point for the application.
+        ///// </summary>
+        //[STAThread]
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new mainForm());
+        //}
+
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new mainForm());
+            if (args == null || args.Length != 2)
+            {
+                printHelp();
+                return;
+            }
+
+            //Write to a PDF format file
+            Console.WriteLine("Converting...");
+            try
+            {
+                bool r = Converter.ConvertTiffToPdf(args[0], args[1]);
+                Console.WriteLine(r ? "Successful completion." : "Failed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Failed: {0}", ex.ToString()));
+            }
+        }
+
+        private static void printHelp()
+        {
+            Console.WriteLine("usage: Txt2Pdf inFilename outFilename");
         }
     }
 }
